61472737775cc3cd6c026eb0f8296830fb012b75
tijlclottemans <tijlclottemans@gmail.com>
2018-05-28 13:17:31 +0200

    created the pages. added a navbar and v1 of the table

--- index.php
+++ index.php
@@ -1,7 +1,64 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tijlclottemans
- * Date: 26/05/2018
- * Time: 16:53
- */
+ // something
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Webshop</title>
+  <link rel="stylesheet" href="css/app.css?">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="banner"></div>
+    <nav class="navbar">
+        <ul>
+            <li><a class="active" href="#">Home</a></li>
+            <li><a href="#">Winkelwagen</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Admin</a></li>
+        </ul>
+    </nav>
+
+    <section class="table">
+        <table>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+            </tr>
+            <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+            </tr>
+            <tr>
+                <td>Ernst Handel</td>
+                <td>Roland Mendel</td>
+                <td>Austria</td>
+            </tr>
+            <tr>
+                <td>Island Trading</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+            </tr>
+            <tr>
+                <td>Laughing Bacchus Winecellars</td>
+                <td>Yoshi Tannamuri</td>
+                <td>Canada</td>
+            </tr>
+            <tr>
+                <td>Magazzini Alimentari Riuniti</td>
+                <td>Giovanni Rovelli</td>
+                <td>Italy</td>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
