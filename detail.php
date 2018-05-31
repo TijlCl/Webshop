639fa293a13485f625f0a2ce77f491e9d80f576b
tijlclottemans <tijlclottemans@gmail.com>
2018-05-31 23:12:30 +0200

    bug fixes

--- detail.php
+++ detail.php
@@ -39,13 +39,14 @@ session_start();
     <?php include_once 'DAO/ProductDAO.php';
     $productdetail = ProductDAO::getById($_GET['q']) ?>
     <h1><?php echo $productdetail->getNaam(); ?></h1>
-    <img class="col-xs-3" src="<?php echo $productdetail->getLocatieFoto(); ?>" width="223" height="310">
+    <img class="col-xs-3" src="<?php echo $productdetail->getLocatieFoto(); ?>" width="187" height="310">
     <p class="col-xs-9"><?php echo $productdetail->getBeschrijving(); ?></p>
     <p class="col-xs-9">Prijs Excl BTW <?php echo '&euro; ' . $productdetail->getPrijsExclBtw(); ?></p>
     <p class="col-xs-9">Totale BTW <?php echo '&euro; ' . $productdetail->getBtw(); ?></p>
     <p class="col-xs-9">Prijs Incl BTW <?php echo '&euro; ' . $productdetail->getPrijsInclBtw(); ?></p>
+    <p class="col-xs-9">Aantal in Stock <?php echo $productdetail->getStock(); ?></p>
     <form class="col-xs-8" action="voegToeAanWinkelwagen.php" method="POST">
-        <input class="col-xs-1" name="aantal" type="number" min="0" value="1">
+        <input class="col-xs-1" name="aantal" type="number" min="0" max="<?php echo $productdetail->getStock(); ?>" value="1">
         <input type="hidden" name="productId" value="<?php echo $productdetail->getProductId(); ?>">
         <button class="col-xs-3" type="submit">In winkelmand!</button>
     </form>
